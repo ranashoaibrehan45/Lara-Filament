b3350fd0f583ed8f0c7b058a7886b9f7aebe5901
Shoaib Rehan <ranashoaibrehan@gmail.com>
2024-04-30 14:23:28 +0500

    first commit

---
+++ app/Filament/Resources/ConferenceResource/Pages/ListConferences.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Resources\ConferenceResource\Pages;
+
+use App\Filament\Resources\ConferenceResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
+
+class ListConferences extends ListRecords
+{
+    protected static string $resource = ConferenceResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            /*'active' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('active', true)),
+            'inactive' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('active', false)),
+            */
+        ];
+    }
+}
